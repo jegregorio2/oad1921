5e5a508b6f7c709be49b553e1931c9cd38ef6ec1
jegregorio2 <juan.e.gregorio@gmail.com>
2022-05-14 00:47:56 -0700

    folders setup

---
+++ README.txt
@@ -0,0 +1,4 @@
+README.txt
+
+Add messages such as
+Put these in xampp
---
+++ index.js
@@ -0,0 +1,7 @@
+// Require left-pad
+const leftPad = require('left-pad');
+// Define output
+const output = leftPad('Hello, World!', 15);
+
+// Send output to the console
+console.log(output);
